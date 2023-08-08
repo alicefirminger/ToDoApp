fb2f23f412ffdc910a468297e38250fbc04102ae
alicefirminger <alicefirminger@gmail.com>
2023-08-08 14:16:22 +0100

    get request and post requests are now working in Thunderclient. Will go back to add notes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "node:fs/promises";
 const app = express();
 const PORT = 3001;
 
@@ -26,10 +27,35 @@ class TaskList {
     }
 }
 
+const taskList = new TaskList();
+
 app.use(express.json());
 
-app.get("/tasks", )
+//GET a list of the tasks
+app.get("/tasks", async (req, res) => {
+    const getTasks = await fs.readFile('tasks.json');
+    const tasks = JSON.parse(getTasks.toString());
+    res.json(tasks);
+});
 
+//POST a new task to the list
+app.post('/tasks', async (req, res) => {
+    const newTask = req.body;
+    const task = new Task(newTask.id, newTask.title);
+    taskList.addTask(task);
+    res.json(task);
+    await fs.writeFile('tasks.json', JSON.stringify(taskList.tasks));
+});
+
+//PUT to toggle a task completed or not
+app.put('/tasks/:taskId/togglecomplete' , (req, res) => {
+
+});
+
+//DELETE a task
+app.delete('/tasks/:taskID' , (req, res) => {
+
+});
 
 app.listen(PORT, function () {
 	console.log(`Server is now listening on http://localhost:${PORT}`);
